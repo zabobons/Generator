1cdb5a08f85117ccb06e773066e303e008f3b31a
zabobons <nadkoszulek@gmail.com>
2014-01-18 13:27:58 +0100

    zlaczenie

--- utils.h
+++ utils.h
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 void print(double* tab)
 {
@@ -21,3 +21,25 @@ double add(double* tab)
 	
 	return suma;
 }
+
+double min(double * tab)
+{
+	enum { SIZE = 10};
+	double min = tab[0];
+	int i;
+	for ( i = 1; i < SIZE; i++)
+		min = tab[i] < min ? tab[i] : min;
+	return min;
+}
+
+
+double* generator()
+{
+	double *tab = (double*)malloc (sizeof(double) * 10);
+	int i;
+	for(i =0;i<10;i++)
+	{
+		tab[i] = rand() % 100;
+	}
+	return tab;
+}	
