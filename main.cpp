efe7b28e39d3928ab9542cc1847c8017e6e64c61
Bartosz Szmit <koxygen7@gmail.com>
2014-01-18 13:20:07 +0100

    Created max

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include "min.h"
+#include "generator.h"
 
 int main()
 {
+	std::cout << min(generator());
 	std::cout << "To jest funkcja main\n";
 	return 0;
 }
--- min.h
+++ min.h
@@ -1,9 +1,10 @@
-double findMin(double * tab)
+double min(double * tab)
 {
 	enum { SIZE = 10};
 	double min = tab[0];
+	int i;
 	for ( i = 1; i < SIZE; i++)
-		tab[i] = tab[i] < min ? tab[i] : min;
+		min = tab[i] < min ? tab[i] : min;
 	return min;
 }
 		
